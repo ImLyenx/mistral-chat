d4775a8ee283d506ccc1ef05d56f8c8d48e37b24
Lyenx <105422453+ImLyenx@users.noreply.github.com>
2025-02-25 10:28:06 +0100

    feat: add basic chat functionnality

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,47 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { useRef, useState } from "react";
+
+import { Mistral } from "@mistralai/mistralai";
 
 export default function Home() {
-  return <Button>test</Button>;
+  const [isLoading, setIsLoading] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const [completion, setCompletion] = useState("");
+  const startCompletion = async () => {
+    setIsLoading(true);
+    const apiKey = inputRef.current?.value;
+    if (!apiKey) {
+      alert("Please enter an API key");
+      return;
+    }
+    const prompt = textareaRef.current?.value;
+    if (!prompt) {
+      alert("Please enter a prompt");
+      return;
+    }
+    const client = new Mistral({ apiKey: apiKey });
+
+    const chatResponse = await client.chat.complete({
+      model: "mistral-tiny",
+      messages: [{ role: "user", content: prompt }],
+    });
+    setCompletion(chatResponse.choices![0].message.content as any);
+    setIsLoading(false);
+  };
+
+  return (
+    <div>
+      <Input ref={inputRef} type="text" />
+      <Textarea ref={textareaRef} id="textarea" />
+      <Button onClick={startCompletion} disabled={isLoading}>
+        {isLoading ? "Loading..." : "Send"}
+      </Button>
+      <p>{completion}</p>
+    </div>
+  );
 }
